837ab9602dd516be917535f875b8a08e7d2d7263
Andrew Cai <andrewca78@gmail.com>
2025-08-04 23:33:03 +0800

    text updates.

--- src/main.js
+++ src/main.js
@@ -523,9 +523,9 @@ const mainContent = `
   
   <p>I'm building things that I hope will help the world. Right now, that’s <a href="https://cameronai.app" target="_blank" rel="noopener noreferrer"><span class="highlight">Cameron</span></a>, a framework that transforms any camera into an AI agent.</p>
   
-  <p>I'm an intern at <a href="https://relixir.ai" target="_blank" rel="noopener noreferrer"><span class="highlight">Relixir (YC X25)</span></a> and study Computer Science & Engineering and Math at UC Irvine. On the side, I’ve done two hackathons and <a href="https://devpost.com/andrewcai8" target="_blank" rel="noopener noreferrer"><span class="highlight">won both</span></a>, earning the grand prize at the world's largest AI Hackathon at UC Berkeley and at Caltech’s Hacktech, winning $16k. Before all that in high school, I built a <a href="https://photos.app.goo.gl/DgvWgtRFLeQZtrvg8" target="_blank" rel="noopener noreferrer"><span class="highlight">CNC machine</span></a> from scratch.</p>
+  <p>I'm an intern at <a href="https://relixir.ai" target="_blank" rel="noopener noreferrer"><span class="highlight">Relixir (YC X25)</span></a> and study Computer Science & Engineering and Math at UC Irvine. On the side, I’ve done two hackathons and <a href="https://devpost.com/andrewcai8" target="_blank" rel="noopener noreferrer"><span class="highlight">won both</span></a>, earning the grand prize at the world's largest AI Hackathon at UC Berkeley and at Caltech’s Hacktech, winning $16k. Before all that in, high school, I built a <a href="https://photos.app.goo.gl/DgvWgtRFLeQZtrvg8" target="_blank" rel="noopener noreferrer"><span class="highlight">CNC machine</span></a> from scratch.</p>
   
-  <p>Outside of code, you’ll find me cafe hopping, walking around, admiring cats, or just reflecting on life. My favorite color is <span class="highlight">#B4472C</span> and listening to Mac Miller keeps me going!</p>
+  <p>Outside of code, you’ll find me cafe hopping, walking around, admiring cats, or just reflecting on life. My favorite color is <span class="highlight">#B4472C</span> and listening the Graduation album keeps me going.</p>
 
   <p>Reach out to me about anything, I'd love to chat! andrewca78[at]gmail[dot]com</p>
 
